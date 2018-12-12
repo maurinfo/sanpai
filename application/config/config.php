e3ee07eb12bdae381b118aacdb364e4ed5f0349a
Jun Maurin <maurinfo@icloud.com>
2018-12-12 17:21:25 +0900

    added prefecture.sql

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/sanpai';
+$config['base_url'] = 'http://192.168.1.9:8888/sanpai';
 
 /*
 |--------------------------------------------------------------------------
