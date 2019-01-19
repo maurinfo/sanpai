d55f91c982ab1b314494852acb716015ebe20846
mcunanan19 <45750449+mcunanan19@users.noreply.github.com>
2019-01-19 13:38:40 +0900

    Update Firm_mod.php
    Set isactive equal to 0 when deleting record instead of deleteing the actual record.

--- application/models/Firm_mod.php
+++ application/models/Firm_mod.php
@@ -2,32 +2,31 @@
 
 class Firm_mod extends CI_Model
 {
-	function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-	}
+    }
 
-	public function insert($data)
-	{
-		if ($this->db->insert("firm", $data)) {
-			return true;
-		}
-	}
+    public function insert($data)
+    {
+        if ($this->db->insert("firm", $data)) {
+            return true;
+        }
+    }
 
-	public function delete($id)
-	{
-		if ($this->db->delete("firm", "id=" . $id)) {
-			return true;
-		}
-	}
+    public function delete($id)
+    {
+        return $this->db->where('id', $id)
+            ->set('isactive', 0)
+            ->update('firm');
+    }
 
-	public function update($id, $data)
-	{
-		$this->db->set($data);
-		$this->db->where("id", $id);
-		$this->db->update("firm", $data);
-	}
+    public function update($id, $data)
+    {
+        $this->db->set($data);
+        $this->db->where("id", $id);
+        $this->db->update("firm", $data);
+    }
 
 }
-?>
