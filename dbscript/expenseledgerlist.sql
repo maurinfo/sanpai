bfb23c384cf3424c60f439e11d71b8b3a9dea787
Jun Maurin <maurinfo@icloud.com>
2019-03-11 14:05:57 +0900

    views

---
+++ dbscript/expenseledgerlist.sql
@@ -0,0 +1,29 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 11, 2019 at 05:02 AM
+-- Server version: 5.7.21
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `kkcl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `expenseledgerlist`
+--
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `expenseledgerlist`  AS  select `accountledger`.`id` AS `id`,`accountledger`.`firmid` AS `firmid`,`accountledger`.`datetransacted` AS `datetransacted`,`accountledger`.`transactiontypeid` AS `transactiontypeid`,`accountledger`.`referenceid` AS `referenceid`,`accountledger`.`amount` AS `amount` from `accountledger` where (`accountledger`.`transactiontypeid` = 3) ;
+
+--
+-- VIEW  `expenseledgerlist`
+-- Data: None
+--
+
