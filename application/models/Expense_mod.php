84b200414d1e08b14e5db4af30b0ab3acda14fe8
ruelyd <ruelydeguzman@gmail.com>
2019-06-21 21:31:14 +0900

    Expense, added search box

--- application/models/Expense_mod.php
+++ application/models/Expense_mod.php
@@ -8,14 +8,36 @@ class expense_mod extends CI_Model
         $this->load->database();
     }
 
-    public function get_expenses($page = 0)
+    public function get_expenses($query,$page = 0)
     {
         return $this->db->order_by("datedelivered", "desc")
             ->where('isactive', 1)
+            ->like('name',$query )
+            ->or_where(($this->checkdatevalidformat($query,"datedelivered")))
+            ->or_like('referenceno',$query )
+            ->or_like('subtotal',$this->clearFormat($query) )
+            ->or_like('tax',$this->clearFormat($query) )
+            ->or_like('total',$this->clearFormat($query) )    
             ->get('expenselist', DEFAULT_PAGE_LIMIT, $page)
             ->result_array();
     }
-
+    
+    
+     public function get_total_record_count($query)
+     {
+   
+         return $this->db->order_by("datedelivered", "desc")
+            ->where('isactive', 1)
+            ->like('name',$query )
+            ->or_where(($this->checkdatevalidformat($query,"datedelivered")))
+            ->or_like('referenceno',$query )
+            ->or_like('subtotal',$this->clearFormat($query) )
+            ->or_like('tax',$this->clearFormat($query) )
+            ->or_like('total',$this->clearFormat($query) )    
+            ->get('salelist')
+           ->num_rows();
+}
+    
     public function get_expense_by_id($id)
     {
         return $this->db
@@ -40,10 +62,35 @@ class expense_mod extends CI_Model
             ->update('expense');
     }
 
-    public function get_total_record_count()
-    {
-        return $this->db
-            ->where('isactive', 1)
-            ->count_all_results('expenselist');
-    }
+    
+ 
+      public function clearFormat($queryString){
+       $rtvalue = "";
+       $regExpression = '/^[0-9,.]+$/';
+
+          if(preg_match($regExpression, $queryString)) {
+            $rtvalue= filter_var($queryString,FILTER_SANITIZE_NUMBER_INT);
+                    return $rtvalue;
+           } else {
+
+           return $queryString;
+     }
+   }
+   
+    function checkdatevalidformat($query,$table){
+
+    if(strtotime($query) == true) {
+       return $rtv = " ".$table." = date('".$query."')";
+     } else {
+       return $table;
+     }
+   }   
+    
+    
+//    public function get_total_record_count()
+//    {
+//        return $this->db
+//            ->where('isactive', 1)
+//            ->count_all_results('expenselist');
+//    }
 }
