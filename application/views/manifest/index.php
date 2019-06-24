9424f0310acb9e28dfe6b94c3cbf2a648467df14
Marvin Cunanan <mcunanan19@gmail.com>
2019-06-24 12:55:09 +0900

    Fixed the pagination position.

--- application/views/manifest/index.php
+++ application/views/manifest/index.php
@@ -72,7 +72,7 @@
             <?php endforeach;?>
          </tbody>
       </table>
-      <div class="panel-body">
+      <div class="panel-body paging-body">
          <div class="mb-15">
             <?=$this->pagination->create_links()?>
          </div>
